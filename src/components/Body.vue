e3f103d63e8bcc71eabefe3e31c8dde8375f0fb3
Trinh Tran <trinhtran@Trinhs-Mac-mini.local>
2022-10-03 17:30:37 +0700

    first commit

---
+++ src/components/Body.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="wrapper">
+    <div class="sidebar"></div>
+    <div class="bg">
+        <span>
+            2022年全日本スーパーフォーミュラ選手権
+        </span>
+        <div>
+            <span>トップ</span>
+            <div class="arrow"></div>
+            <span>レース一覧</span>
+            <div class="arrow"></div>
+            <span>2022年全日本スーパーフォーミュラ選手権</span>
+        </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.wrapper{
+    display: flex;
+    justify-content: center;
+    width: 1400px;
+    height: 1919px;
+    padding: 0 0 234px;
+    background-color: #ededed;
+}
+.sidebar{
+    width: 316px;
+    background-color: #202529;
+    height: 300px;
+}
+.bg{
+    width: 862px;
+    height: 82px;
+    padding: 16px 24px;
+    border-radius: 8px;
+    background-color: #202529;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <main>
+    <Header/>
+    <Body/>
+  </main>
+</template>
+
+<script>
+import Header from '../components/Header.vue'
+import Body from '../components/Body.vue'
+export default {
+    components: {Header, Body}
+
+}
+</script>
+
+<style>
+</style>
